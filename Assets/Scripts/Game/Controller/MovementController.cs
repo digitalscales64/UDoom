5ce1c31682eb66535573f136f32922967d96760f
digitalscales64 <digitalscalessynth@gmail.com>
2024-09-18 19:28:19 -0500

    Rework
    Changed to character controller

--- Assets/Scripts/Game/Controller/MovementController.cs
+++ Assets/Scripts/Game/Controller/MovementController.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 
 namespace Game.Controller 
 {
-    public class MovementController : MonoBehaviour, Core.IMovement
+    public class MovementController : MonoBehaviour
     {
-        private const int MAX_NEIGHBOURS = 16;
         private const float GRAVITY = 9.8f;
-        private const float EPSILON = 0.001f;
-        private const int MAX_ITERATTIONS = 18;
+        private const float TOLERANCE = 0.01f;
 
         #region Serialized Fields
 
         [SerializeField] private MovementAttributes _attributes;
-        [SerializeField] private CapsuleCollider _collider;
-        [SerializeField] private LayerMask _layerMask;
+        [SerializeField] private CharacterController _controller;
         [SerializeField] private bool _useGravity;
 
         #endregion
-
-        private Action<GameObject> _onCollision;
-        private Action<bool> _onFalling;
 
         // Desired Movement
         private Vector3 _moveDirection;
@@ -32,11 +26,9 @@ namespace Game.Controller
         private Vector3 _turnDirection;
         private float _turnMagnitude;
 
-        // Collisions
-        private Collider[] _neighbours;
-
-        // Falling
+        // gravity
         private float _gravity;
+        private bool _falling;
 
         void Start() 
         {
@@ -46,14 +38,12 @@ namespace Game.Controller
             IRotationInput rotationInput = GetComponent<IRotationInput>();
             rotationInput.AddRotationListener(SetRotation); 
 
-            if(!_collider) 
+            if(!_controller) 
             {
-                _collider = GetComponent<CapsuleCollider>(); 
-                string msg = "Capsule collider not set for MovementController";
+                _controller = GetComponent<CharacterController>(); 
+                string msg = "No CharacterController present";
                 Debug.LogWarning(msg);
             }
-
-            _neighbours = new Collider[MAX_NEIGHBOURS];
         }
 
         void FixedUpdate() 
@@ -62,112 +52,55 @@ namespace Game.Controller
             Movement();
         }
 
-        protected virtual void Movement() 
+        void Movement() 
         {
             // Desired Movement
-            float impulse = _moveMagnitude * _attributes.MovementSpeed * Time.fixedDeltaTime;
-            Vector3 advance = _moveDirection * impulse;
-
-            // Gravity
-            Vector3 gravity = Vector3.down * _gravity;
-
-            int count = Physics.OverlapCapsuleNonAlloc
-            (
-                transform.position + Vector3.up * _collider.height/2.0f,
-                transform.position - Vector3.up * _collider.height/2.0f,
-                _collider.radius + 2 * EPSILON, _neighbours,
-                _layerMask, QueryTriggerInteraction.Ignore 
-            );
-
-            bool isGrounded = false;
-            Vector3 movement = transform.position + advance + gravity;
-
-            for(int iteration = 0; iteration < MAX_ITERATTIONS; iteration++) 
+            Vector3 advance = _moveDirection * (_moveMagnitude * _attributes.MovementSpeed * Time.fixedDeltaTime) + Vector3.down * _gravity;
+            
+            if(_controller.isGrounded) 
             {
-                // Depenetration
-                Vector3 resolve = Vector3.zero;
-
-                for(int i = 0; i < count; i++) 
+                _falling = false;
+            }
+            else 
+            {
+                RaycastHit hit;
+                Vector3 next = transform.position + advance;
+                if(Physics.SphereCast(next, 2.0f, Vector3.down, out hit)) 
                 {
-                    Collider other = _neighbours[i];
-                    if(_collider == other) continue;
-
-                    Vector3 otherPosition = other.gameObject.transform.position;
-                    Quaternion otherRotation = other.gameObject.transform.rotation; 
-
-                    _onCollision?.Invoke(other.gameObject);
-
-                    Vector3 direction;
-                    float distance;
-
-                    bool overlapped = Physics.ComputePenetration
-                    (
-                    _collider, movement, transform.rotation,
-                    other, otherPosition, otherRotation,
-                    out direction, out distance
-                    );
-
-                    if(!overlapped) continue;
-
-                    resolve += direction * (distance - EPSILON);
-
-                    float dot = Vector3.Dot(Vector3.up, direction);
-                    if(dot < 0.5f) continue;
-
-                    RaycastHit hit;
-                    if(Physics.Raycast(movement, -direction, out hit)) 
+                    float distance = next.y - hit.point.y;
+                    
+                    if(distance <= _controller.height/2.0f + TOLERANCE) 
                     {
-                        isGrounded = true;
+                        Vector3 down = Vector3.down * distance;
+                        advance += down;
+
+                        _falling = false;
                     }
                 }
-
-                movement += resolve;
+                else 
+                {
+                    _falling = true;
+                }
             }
 
-            // Grounding
-            _onFalling?.Invoke(!isGrounded);
-
-            if(!isGrounded) 
+            if(_falling) 
             {
                 _gravity += GRAVITY * Time.fixedDeltaTime * Time.fixedDeltaTime;
             }
             else 
-            {
+            {   
                 _gravity = 0.0f;
             }
 
-            // Advance is the desired movement, and resolve is a vector stopping the advance
-            transform.position = movement;
+            _controller.Move(advance);
         }
 
-        protected virtual void Rotation() 
+        void Rotation() 
         {
             float speed = _turnDirection.x * _turnMagnitude * _attributes.TurnSpeed * Time.deltaTime;
             Vector3 rot = new Vector3(0.0f, speed, 0.0f);
             
             transform.Rotate(rot);
-        }
-
-        public void AddFallingListener(Action<bool> eventHandler) 
-        {
-            _onFalling += eventHandler;
-        }
-
-        public void RemoveFallingListener(Action<bool> eventHandler) 
-        {
-            _onFalling -= eventHandler;
-        }
-
-        #region IMovement Implementation
-
-        public void AddCollisionListener(Action<GameObject> eventHandler) 
-        {
-            _onCollision += eventHandler;
-        }
-
-        public void RemoveCollisionListener(Action<GameObject> eventHander) 
-        {
-            _onCollision -= eventHander;
         }
 
         public void SetMovement(Vector3 dir, float mag)
@@ -181,7 +114,5 @@ namespace Game.Controller
             _turnDirection = dir;
             _turnMagnitude = mag;
         }
-
-        #endregion
     }
 }
